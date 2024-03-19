fed64931d5b50613cab4046d2c96b81dd1017bb5
TalsCodingArea <139641338+TalsCodingArea@users.noreply.github.com>
2024-03-19 11:19:22 +0200

    Final update to the RFC-updater

--- Find-missing-RS-logs/RFC-updater.ps1
+++ Find-missing-RS-logs/RFC-updater.ps1
@@ -1,18 +1,19 @@
 #DECLERATIONS
 $rfcInstallerPath = "C:\DDA\station\log\rfc-installer.log"
 $chPath = "C:\DDA\station\CH.PRM"
-$procPath = "C:\DDA\station\RFC_PROC"
-$iniPath =  "C:\DDA\station\RFC_3000"
+$procPath = "C:\DDA\station\RFC_PROC.INI"
+$iniPath =  "C:\DDA\station\RFC_3000.INI"
 $prmPath = "C:\DDA\station\STATION.PRM"
 #END DECLERATIONS
 
 #Step 1: Check for "FUEL4" line on the CH.PRM file
 $chContent = Get-Content -Path $chPath
 $managerFlag = $false
+$linesToAdd = "210705 FUEL4" + "`n"
 for ($i = 0; $i -lt $chContent.Length; $i++) {
     if ($chContent[$i] -match "MANAGER" -and $managerFlag -eq $false) {  #Checks if we are on the MANAGER section
         $managerFlag = $true
-        break
+        continue
     }
     if ($chContent[$i] -match "MANAGER" -and $managerFlag -eq $true) { #Skip all the MANAGER lines
         continue
@@ -25,9 +26,12 @@ for ($i = 0; $i -lt $chContent.Length; $i++) {
             Write-Output "FUEL4 line found on CH.PRM file"
             break
         }
-        $chContent = $chContent[0..$i-1] + "210705 FUEL4" + "`r`n" + $chContent[$i..$chContent.Length - 1] #Add the FUEL4 line
+        $chContent = $chContent[0..($i-1)] + "210705 FUEL4" + "`n" + $chContent[$i..($chContent.Length - 1)] #Add the FUEL4 line
+        break
     }
 }
+Set-Content -Path $chPath -Value $chContent
+Write-Output "Step 1 completed"
 
 #Step 2: Validate process 20 and process 30 on RFC_PROC
 $procContent = Get-Content -Path $procPath
@@ -41,49 +45,29 @@ for ($i = 0; $i -lt $procContent.Length; $i++) {
             continue
         }
         else { #Change the lines to be commented
-            $newLines += 1
-            $newLines += $i
-            $newLines += "//" + $procContent[$i]
-            $i++
-            $newLines += "//" + $procContent[$i]
-            $i++
-            $newLines += "//" + $procContent[$i]
-            $i++
-            $newLines += "//" + $procContent[$i]
-        }
+            for($j=0; $j -lt 4; $j++) {
+                $procContent[$i] = "//" + $procContent[$i]
+                $i++
+            }
+        Write-Output "Added the comments for process 20"
         continue
+        }
     }
     if ($procContent[$i] -match "PROCESS 30") {
         $i++
         if ($procContent[$i] -match "^//") { #Remove the comment if the line after process 30 is commented
-            $newLines[0] += 1
-            $newLines += $i
-            $procContent[$i].replace("//", "")
-            $i++
-            $procContent[$i].replace("//", "")
-            $i++
-            $procContent[$i].replace("//", "")
-            $i++
-            $procContent[$i].replace("//", "")
+            for($j=0; $j -lt 5; $j++) {
+                $procContent[$i] = $procContent[$i].Substring(2)
+                $i++
+            }
+            Write-Output "Removed the comment from process 30"
             break
         }
         break
     }
 }
-#Settin the new lines
-if ($newLines[0] -eq 1) {
-    for ($i = 2; $i -lt 6; $i++) {
-        $procContent[$newLines[1]] = $newLines[$i]
-        $newLines[1]++
-    }
-}
-elseif ($newLines[0] -eq 2) {
-    for ($i = 7; $i -lt 11; $i++) {
-        $procContent[$newLines[6]] = $newLines[$i]
-        $newLines[6]++
-    }
-}
 Set-Content -Path $procPath -Value $procContent
+Write-Output "Step 2 completed"
 
 #Step 3: Validate RFC_3000
 $iniContent = Get-Content -Path $iniPath
@@ -101,16 +85,28 @@ for ($i = 0; $i -lt $iniContent.Length; $i++) {
     }
 }
 Set-Content -Path $iniPath -Value $iniContent
+Write-Output "Step 3 completed"
 
 #Step 4: Validate STATION.PRM
 $prmContent = Get-Content -Path $prmPath
 for ($i = 1; $i -lt $prmContent.Length; $i++) {
-    if (-not($prmContent[$i].Substring(73, 2) -match "8")) {
-        continue
+    if(-not(($prmContent[$i].Substring(73, 2)) -match "\d")){
+        Write-Output "line $i does not have an integer"
+        continue #Skip the lines that doesn't have a transaction number
     }
-    else {
-        $tmp = [int]$prmContent[$i].Substring(73, 2) + 8
-        $prmContent[$i] = $prmContent[$i].Substring(0, 73) + $tmp + $prmContent[$i].Substring(75)
+    else{
+        $Spc = [int]($prmContent[$i].Substring(73, 2))
+        if ($Spc -eq 8) {
+            continue
+        }
+        else {
+            $Spc = $Spc + 8
+            $prmContent[$i] = $prmContent[$i].Substring(0, 73) + $spc + $prmContent[$i].Substring(75)
+            Write-Output "The line is updated"
+        }
     }
 }
 Set-Content -Path $prmPath -Value $prmContent
+Write-Output "The new content has been set"
+Write-Output "Step 4 completed"
+Start-Sleep -Seconds 5
