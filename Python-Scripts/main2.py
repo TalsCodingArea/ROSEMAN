1a20f5cd77af18098b0f6ddd6fcd67f9bae87e56
TalsCodingArea <139641338+TalsCodingArea@users.noreply.github.com>
2024-05-11 23:08:26 +0300

    Created a new way to examine differences Now you don't need to enter the date of the difference and it will look through all the rs file from the last 10 days This way we don't need to check if there was actually a difference and automatically run this exe file

--- Python-Scripts/main2.py
+++ Python-Scripts/main2.py
@@ -1,30 +1,33 @@
 import os
 from datetime import datetime,timedelta
+import shutil
 
 ########################## Variables ##########################
 
-rs_path = "C:/DDA/station/data/RS050524.D1A"   # The path to the RS file
+date = (datetime.today() - timedelta(days=10)).strftime('%d%m%y')
+rs_path = "C:/DDA/station/data/RS" + date + ".D1A"   # The path to the RS file
 DLG_path = "C:/DDA/station/log/DLG"  # The path to the DLG file
 station_path = "C:/DDA/station/STATION.PRM" #The path to the station.prm file
 
 ########################## Functions ##########################
 
-def open_close(rs_path, pump, nzl):
+def is_opened(rs_path, pump, nzl):
     if os.path.isfile(rs_path):
-        first_line = True
-        opened = False
         with open(rs_path, 'r', errors="ignore") as rs:
             for line in rs:
-                if first_line:
-                    first_line = False
-                    continue
-                if ("Open" in line):
-                    if (int(line.split()[2])==nzl and int(line.split()[3])==pump):
-                        opened = True
-                    continue
-                if ("Close" in line and opened):
-                    if (int(line.split()[2])==nzl and int(line.split()[3])==pump):
-                        return True
+                if ("Open" in line and int(line.split()[3])==pump and int(line.split()[2])==nzl):
+                    return True
+            return False
+    else:
+        print("RS file was not found")
+        return None
+    
+def is_closed(rs_path, pump, nzl):
+    if os.path.isfile(rs_path):
+        with open(rs_path, 'r', errors="ignore") as rs:
+            for line in rs:
+                if ("Close" in line and int(line.split()[3])==pump and int(line.split()[2])==nzl):
+                    return True
             return False
     else:
         print("RS file was not found")
@@ -52,7 +55,7 @@ def pump_difference(rs_path, pump, nzl):
                 if "Open" in line:
                     if(int(line.split()[2]) == nzl and int(line.split()[3]) == pump and counter == 0): #This is the first open for the pump and nzl
                         counter = float(line.split()[0])
-                elif (int(line.split()[3]) == nzl and int(line.split()[4]) == pump): #Finds that the pump and nzl has been opened and initializes the counter count
+                elif (not "Close" in line and int(line.split()[3]) == nzl and int(line.split()[4]) == pump): #Finds that the pump and nzl has been opened and initializes the counter count
                     counter += float(line.split()[0])
                     continue
         with open(rs_path, 'r', errors="ignore") as rs:
@@ -69,14 +72,14 @@ def pump_difference(rs_path, pump, nzl):
                         close_money_counter = float(close_money_counter)/100
                         str = str(counter - close_money_counter)
                         index = str.index('.')
-                        if len(str) - index > 3:
+                        if (len(str) - index) > 3:
                             str = str[:index+2]
                         if abs(counter - close_money_counter) > 1:
                             print("Pump: " + str(pump) + " NZL: " + str(nzl) + " has a difference of " + str + " in the counter")
                             return True, (counter - close_money_counter)
                         else:
                             return False, 0.0
-            return False, 999999
+            return True, 999999
     else:
         print("RS file was not found")
         return None
@@ -104,35 +107,38 @@ def get_closing_time(rs_path):
         print("RS file was not found")
         return None
 
-def close_pump(rs_path, pump, nzl, counter):
-    new_rs_path = next_rs(rs_path)
-    if os.path.isfile(new_rs_path):
+def close_pump(rs_path, pump, nzl):
+    if not os.path.exists("C:/DDA/station/data/backup"):
+        os.makedirs("C:/DDA/station/data/backup")
+    shutil.copy(rs_path, "C:/DDA/station/data/backup")
+    next_rs_path = next_rs(rs_path)
+    if os.path.isfile(next_rs_path):
         first_line = True
-        with open(new_rs_path, 'r', errors="ignore") as rs:
-            for line in rs:
+        with open(next_rs_path, 'r', errors="ignore") as next_rs:
+            for line in next_rs:
                 if first_line:
                     first_line = False
                     continue
                 if ("Open" in line):
-                    if (int(line.split()[2])==nzl and int(line.split()[3])==pump and (abs(float(line.split()[0])-counter)<1)):
+                    if (int(line.split()[2])==nzl and int(line.split()[3])==pump):
                         with open('rs_path', 'a') as rs:
                             line_to_write = line.replace("Open ", "Close")
                             line_to_write = line_to_write.replace("000000", "999999")
                             line_to_write = line_to_write.replace(line.split()[5], get_closing_time(rs_path)[0])
-                            rs.write(line)
                             line_to_write = line_to_write.replace(line.split()[6], get_closing_time(rs_path)[1])
                             rs.write(line_to_write)
+                            print("Pump: " + str(pump) + " and nozzle: " + str(nzl) + " has been closed")
                             return True
                 if ("Close" in line):
-                    if (int(line.split()[2])==nzl and int(line.split()[3])==pump and (abs(float(line.split()[0])-counter)<1)):
+                    if (int(line.split()[2])==nzl and int(line.split()[3])==pump):
                         with open('rs_path', 'a') as rs:
                             line_to_write = line.replace(line.split()[5], get_closing_time(rs_path)[0])
                             line_to_write = line_to_write.replace(line.split()[6], get_closing_time(rs_path)[1])
                             rs.write(line_to_write)
+                            print("Pump: " + str(pump) + " and nozzle: " + str(nzl) + " has been closed")
                             return True
             print("Couldn't find a closing line for pump: " + str(pump) + " and nozzle: " + str(nzl))
             print("You'll have to close the pump manually...")
-            print("There's a probability that the pump was closed and has another difference in the counter")
             return False
     else:
         print("Couldn't find the next RS and so closing the pump was not successful")
@@ -147,26 +153,44 @@ def negative_difference(rs_path, pump, nzl):
     return None
 
 ########################## Main ##########################
-
+print ("Hello, I'm the pump counter checker")
+print ("Starting to examine RS files from the last 10 days")
 if os.path.isfile(station_path):
     with open(station_path, 'r', errors="ignore") as station_file:
-        first_line = True
-        for line in station_file:
-            if first_line:
-                first_line = False
-                continue
-            if (line.split()[0].isdigit() and line.split()[4].isdigit()):
-                pump = int(line.split()[0])
-                nzl = int(line.split()[4])
-                is_difference, difference = pump_difference(rs_path, pump, nzl)
-                if(is_difference):
-                    print("Pump: " + str(pump) + " NZL: " + str(nzl) + " has a difference in the counter of " + str(difference)) 
-                    if(difference>300):
-                        close_pump(rs_path, pump, nzl, difference)
-                    elif(difference>0):
-                        positive_difference(rs_path, pump, nzl)
-                    elif(difference<0):
-                        negative_difference(rs_path, pump, nzl)
+        while os.path.isfile(rs_path):
+            first_line = True
+            for line in station_file:
+                if first_line:
+                    first_line = False
+                    continue
+                if (line.split()[0].isdigit() and line.split()[4].isdigit()):
+                    pump = int(line.split()[0])
+                    nzl = int(line.split()[4])
+                    is_difference, difference = pump_difference(rs_path, pump, nzl)
+                    if(is_difference):
+                        print("I've found a difference in rs file: " + rs_path)
+                        print("Pump: " + str(pump) + " NZL: " + str(nzl) + " has a difference in the counter of " + str(difference))
+                        input = " "
+                        while input!="Y" and input!="y" and input!="N" and input!="n":
+                            input = input("Would you like to proceed with this difference? (Y/N)")
+                            if (input == 'Y' or input == 'y'):
+                                if(difference==999999):
+                                    print("It seems like there a counter difference")
+                                    print("Let me check if the pump and nozzle was opened and closed")
+                                    if (is_opened(rs_path, pump, nzl)):
+                                        print("The pump and nozzle has been opened")
+                                    if (is_closed(rs_path, pump, nzl)):
+                                        print("The pump and nozzle has been closed")
+                                    else:
+                                        close_pump(rs_path, pump, nzl, difference)
+                                elif(difference>0):
+                                    positive_difference(rs_path, pump, nzl)
+                                elif(difference<0):
+                                    negative_difference(rs_path, pump, nzl)
+            rs_path = next_rs(rs_path)
 else:
     print("Couldn't find the station file in the path: " + station_path)
 input()
+
+
+
--- Python-Scripts/test.py
+++ Python-Scripts/test.py
@@ -11,3 +11,13 @@ rs_path = "C:/DDA/station/data/RS050524.D1A"
 date = rs_path[-10:-4]
 next_rs = rs_path[:-10] + get_tomorrow(date) + rs_path[-4:]
 print(next_rs)
+print (rs_path[-1])
+
+date = (datetime.today() - timedelta(days=10)).strftime('%d%m%y')
+print (date)
+input = input("Would you like to proceed with this difference? (Y/N)")
+print (input)
+path = "/path/to/your/directory"  # replace with your actual path
+folder_name = "backup"
+full_path = os.path.join(path, folder_name)
+print (full_path)
